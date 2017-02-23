2a0e551c2ba64bd435f7996d7c0aa82f662efc26
Oleksii.rud <tarnit@gmail.com>
2017-02-23 15:37:03 +0100

    Add Get-UserTelephones.ps1

---
+++ Scripts/Get-UserTelephones.ps1
@@ -0,0 +1,2 @@
+﻿ipmo activedirectory
+Get-ADUser -filter {enabled -eq $true -and telephoneNumber -like "*"} -properties telephonenumber, department, displayname | select DisplayName, Department, telephonenumber | Out-GridView -Wait
