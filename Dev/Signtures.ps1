1aaa9264fedc013c273629d11462672754f99ba7
Aleksey Rud <tarnit@gmail.com>
2017-08-21 16:08:05 +0200

    Redesigned Signatures creating script

--- Dev/Signtures.ps1
+++ Dev/Signtures.ps1
@@ -1,33 +1,49 @@
 param(
-    [string]$TemplatePath = 'C:\Work\signatur.html',
-    [string]$OutputFolder = 'C:\Work\Signatures'
+    [string]$htmTemplatePath = '\\ds.net\share\PrgData\Signatures\template\signatur.html',
+    [string]$txtTemplatePath = '\\ds.net\share\PrgData\Signatures\template\signatur.txt',
+    [string]$OutputFolder = '\\ds.net\share\PrgData\Signatures'
 )
-$template = get-content $TemplatePath -encoding UTF8
+$htmtemplate = get-content $htmTemplatePath -encoding UTF8
+$txttemplate = [io.file]::ReadAllText($txtTemplatePath,[text.encoding]::UTF8)
 $Userdata = Get-ADUser -filter {enabled -eq $true} -SearchBase "OU=Site,DC=ds,DC=net" -Properties mail, telephoneNumber, mobile, Title | Select-Object Surname, GivenName, Title, telephoneNumber, mobile, mail, samaccountname
-
+$regex = [regex]"(%%)([^%]+)(%%)"
 foreach ($user in $UserData) {
-    $NewFileText  = $template
-    $NewFileName = "$($user.samaccountname).htm"
+    $Replacer = {
+        param(
+            [System.Text.RegularExpressions.Match]$match
+        )
+        $tag = $match.Value
+        $TagName = $tag.Trim("%")
+        [string]$($repl.$TagName)
+        }
+    $NewhtmFileName = "$($user.samaccountname).htm"
+    $NewTxtFileName = "$($user.samaccountname).txt"
     if ($([string]$user.telephoneNumber).trim() -ne ""){
         $phone = "T: $($user.telephonenumber)<br>"
+        $phonetext = "T: $($user.telephonenumber)`r`n"
     } else{
         $phone = ""
+        $phonetext = ""
     }
     if ($([string]$user.mobile).trim() -ne ""){
         $Mobile = "M: $($user.mobile)<br>"
+        $Mobiletext = "M: $($user.mobile)`r`n"
     } else{
         $Mobile = ""
+        $Mobiletext = ""
     }
-    $repl = @{
-        '%%FirstName%%'   = $user.GivenName
-        '%%LastName%%'    = $user.Surname
-        '%%title%%'       = $user.title
-        '%%TPhoneNumber%%' = $phone
-        '%%MPhoneNumber%%' = $Mobile
-        '%%Email%%'       = $user.mail
+    $script:repl = @{
+        'FirstName'   = $user.GivenName
+        'LastName'    = $user.Surname
+        'title'       = $user.title
+        'TPhoneNumber' = $phone
+        'MPhoneNumber' = $Mobile
+        'PhoneNumber' = $phonetext
+        'MobileNumber' = $Mobiletext
+        'Email'       = $user.mail
     }
-    foreach ($key in $repl.Keys) {
-        $NewFileText = $NewFileText.replace($key, $repl[$key])
-    }
-    $NewFileText | Set-Content -Path $(Join-Path $OutputFolder $NewFileName) -Encoding UTF8
+    $NewFilehtm = $regex.Replace($htmtemplate, $Replacer)
+    $NewFilehtm | Set-Content -Path $(Join-Path $OutputFolder $NewhtmFileName) -Encoding UTF8
+    $NewFiletxt = $regex.Replace($txttemplate, $Replacer)
+    $NewFiletxt | Set-Content -Path $(Join-Path $OutputFolder $NewTxtFileName) -Encoding UTF8
 }
