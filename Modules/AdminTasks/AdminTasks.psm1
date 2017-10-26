1dd817658593a7d9dd0cc01e5b0d94f7acd9283a
Aleksey Rud <tarnit@gmail.com>
2017-10-26 16:40:47 +0200

    Admin tasks: added comment based help. Function Read-SfBClientLog renamed to Import-SfBClientLog

--- Modules/AdminTasks/AdminTasks.psm1
+++ Modules/AdminTasks/AdminTasks.psm1
@@ -1,4 +1,20 @@
 Function Connect-ExchangeOnline {
+    <#
+    .SYNOPSIS
+    Establish connection to exchange online
+    
+    .DESCRIPTION
+    Establish connection to exchange online and downloads powershell cmdlets
+
+    .PARAMETER Credential
+    PSCredential to be used during connection
+    
+    .EXAMPLE
+    PS> Connect-ExchangeOnline
+    
+    Will ask for credential and use it to connect to Exchange online services
+
+    #>
     [CmdletBinding()]
     param(
         [pscredential]$Credential = $(Get-Credential)
@@ -9,6 +25,31 @@ Function Connect-ExchangeOnline {
 }
 
 Function ConvertTo-NormalizedPhoneNumber {
+    <#
+    .SYNOPSIS
+    Converts phone number to normalized form
+    
+    .DESCRIPTION
+    Converts phone number to normalized form 
+    
+    .PARAMETER PhoneNumber
+    Phone number to be converted
+    
+    .PARAMETER LocalPrefix
+    Country prefix to add to local numbers
+    
+    .EXAMPLE
+    ConvertTo-NormalizedPhoneNumber 015855555555
+    +4915855555555
+
+    .EXAMPLE
+    PS> ConvertTo-NormalizedPhoneNumber 004915855555555
+    +4915855555555
+    
+    .EXAMPLE
+    PS> ConvertTo-NormalizedPhoneNumber +4915855555555
+    +4915855555555
+    #>
     [CmdletBinding()]
     param(
         [string]$PhoneNumber,
@@ -21,6 +62,19 @@ Function ConvertTo-NormalizedPhoneNumber {
 }
 
 Function Get-PlacetelNumbers {
+    <#
+    .SYNOPSIS
+    Get list of numbers provided by PlaceTel
+    
+    .DESCRIPTION
+    Uses PlaceTel API to get list of provided numbers
+    
+    .PARAMETER ApiKey
+    API key used to authenticate at placetel. API key is available at placetel website
+    
+    .EXAMPLE
+    PS> Get-PlacetelNumbers -ApiKey lfjghlksdjhfglksdbfglhoghlkbglsdbfgkldjgfsdhfgsbdnf
+    #>
     [CmdletBinding()]
     param(
         [string]$ApiKey
@@ -36,6 +90,24 @@ Function Get-PlacetelNumbers {
 }
 
 Function Get-LineUriPhoneNumber {
+    <#
+    .SYNOPSIS
+    Extract phone number from phone uri
+    
+    .DESCRIPTION
+    Extract phone number from phone uri. Accepts input from pipeline
+    
+    .PARAMETER LineUri
+    Phone uri string
+    
+    .EXAMPLE
+    PS> Get-LineUriPhoneNumber -LineUri "tel:+4915855555555;ext=555"
+    +4915855555555
+    
+    .EXAMPLE
+    PS> Get-csUser contoso\john.doe | Get-LineUriPhoneNumber
+    +4915855555555
+    #>
     [CmdletBinding()]
     param([parameter(ValueFromPipeline, ValueFromPipelineByPropertyName)][string]$LineUri)
     Begin {
@@ -48,13 +120,43 @@ Function Get-LineUriPhoneNumber {
 }
 
 Function Convert-NumberRangeToArray {
+    <#
+    .SYNOPSIS
+    Converts number range boundaries to array of contained numbers
+    
+    .DESCRIPTION
+    Converts number range boundaries to array of contained numbers. Numbers can normalized, non-normalized or phone uri. Output will be in the same format as Starting number
+    
+    .PARAMETER NumberRangeStart
+    First number in range
+        
+    .PARAMETER NumberRangeEnd
+    Last number in range
+    
+    .EXAMPLE
+    PS> Convert-NumberRangeToArray -NumberRangeStart "+4915855555555" -NumberRangeEnd "+4915855555559"
+    +4915855555555
+    +4915855555556
+    +4915855555557
+    +4915855555558
+    +4915855555559
+
+    .EXAMPLE
+    PS > Convert-NumberRangeToArray -NumberRangeStart "tel:+4915855555555" -NumberRangeEnd "tel:+4915855555559"
+    tel:+4915855555555
+    tel:+4915855555556
+    tel:+4915855555557
+    tel:+4915855555558
+    tel:+4915855555559  
+    
+    #>
     [CmdletBinding()]
     param(
         [parameter (Mandatory = $true, ValueFromPipelineByPropertyName)][string]$NumberRangeStart,
         [parameter (Mandatory = $true, ValueFromPipelineByPropertyName)][string]$NumberRangeEnd
     )
     Begin {
-        $r = [regex]"(tel:)(\+)?([0]+)?([\d]+)"
+        $r = [regex]"(tel:)?(\+)?([0]+)?([\d]+)"
     }
     process {
         $mstart = $r.Match($NumberRangeStart)
@@ -70,6 +172,22 @@ Function Convert-NumberRangeToArray {
 }
 
 Function Get-SfBUsedNumbers {
+    <#
+    .SYNOPSIS
+    Returns list of all numbers used by Skype for Business
+    
+    .DESCRIPTION
+    Returns array of all numbers assigned to user, dial-in numbers or saved as unassigned number
+    
+    .PARAMETER ExcludeUnassignedNumbers
+    If specified Unassigned numbers will not be returned
+    
+    .EXAMPLE
+    Get-SfBUsedNumbers
+    
+    .NOTES
+    General notes
+    #>
     [CmdletBinding()]
     param([switch]$ExcludeUnassignedNumbers)
     #User Numbers
@@ -83,6 +201,19 @@ Function Get-SfBUsedNumbers {
 }
 
 Function Get-FreePhoneNumbers {
+    <#
+    .SYNOPSIS
+    Returns list of not used phone numbers for Skype for Business
+    
+    .DESCRIPTION
+    Returns list of not used phone numbers provided by Placetel, but not used for user, dial-in conferencing or unassigned call forwarding
+    
+    .PARAMETER PlaceTelApiKey
+    API key used to authenticate at placetel. API key is available at placetel website
+    
+    .EXAMPLE
+    Get-FreePhoneNumbers -PlaceTelApiKey asdlfhkadkjsbfkjahsdfkldaslfkgdhjasgflkdasbflasdfghaojdsfjasdg
+    #>
     [CmdletBinding()]
     param ([parameter (Mandatory = $true)][string]$PlaceTelApiKey)
     $AllNumbers = Get-PlacetelNumbers -ApiKey $PlaceTelApiKey
@@ -91,6 +222,19 @@ Function Get-FreePhoneNumbers {
 }
 
 Function Get-WrongSfBNumberConfig {
+<#
+.SYNOPSIS
+Returns Skype for Business users, who has incorrect phone numbers
+
+.DESCRIPTION
+Returns Skype for Business users, who has incorrect phone numbers (Not available for Skype for business)
+
+.PARAMETER PlaceTelApiKey
+API key used to authenticate at placetel. API key is available at placetel website
+
+.EXAMPLE
+Get-WrongSfBNumberConfig -PlaceTelApiKey asdlfhkadkjsbfkjahsdfkldaslfkgdhjasgflkdasbflasdfghaojdsfjasdg
+#>
     [CmdletBinding()]
     param([parameter (Mandatory = $true)][string]$PlaceTelApiKey)
     $AllNumbers = Get-PlacetelNumbers -ApiKey $PlaceTelApiKey
@@ -98,6 +242,30 @@ Function Get-WrongSfBNumberConfig {
 }
 
 Function Set-SfBSecondaryNumber {
+    <#
+    .SYNOPSIS
+    Creates secondary number for user
+    
+    .DESCRIPTION
+    Creates unassigned phone number and if required announcement, to forward calls to specified user
+    
+    .PARAMETER Identity
+    Skype for business user identity. Can be
+        -- csUser object;
+        -- the user's SIP address;
+        -- the user's user principal name (UPN);
+        -- the user's domain name and logon name, in the form domain\logon (for example, litwareinc\kenmyer);
+        -- the user's Active Directory display name (for example, Ken Myer). You can also reference a user account by using the user's Active Directory distinguished name.
+    
+    .PARAMETER LineUri
+    Phone number to be used. The line Uniform Resource Identifier (URI) must be specified using the E.164 format and use the "TEL:" prefix. For example: TEL:+14255551297
+    
+    .EXAMPLE
+    Set-SfBSecondaryNumber -Identity contoso\john.doe -LineUri tel:+4915855555555
+    
+    .NOTES
+    General notes
+    #>
     [CmdletBinding()]
     param(
         [parameter (Mandatory = $true, ValueFromPipelineByPropertyName)]$Identity,
@@ -122,7 +290,20 @@ Function Set-SfBSecondaryNumber {
     }
 }
 
-function Read-SfBClientLog {
+function Import-SfBClientLog {
+    <#
+    .SYNOPSIS
+    Parses Skype for Business client log  
+    
+    .DESCRIPTION
+    Imports Skype for business client log file to object array, which is easier to analyse
+    
+    .PARAMETER Path
+    Path to log file
+    
+    .EXAMPLE
+    Import-SfBClientLog c:\admin\sfbmac.log
+    #>
     [CmdletBinding()]
     param (
         [string]$Path
